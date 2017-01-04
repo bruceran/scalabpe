a84d6bf275424be981c48674e8df4b31df0e08fa
bruceran <bruce_ran@sina.com>
2017-01-04 15:54:45 +0800

    v1.1.12

--- third_party/activemq/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set pluginname=activemq
-set pluginversion=1.0.0
-
-mkdir classes\jvmdbbroker\plugin\  2>>nul 1>>nul
-call scalac -deprecation -encoding "UTF-8" -classpath "lib\*;..\..\lib\*;.\*"  -d "./classes"  ./src/*.scala
-copy /Y src\jvmdbbroker.plugins.conf .\classes\   2>>nul 1>>nul
-copy /Y src\release_notes.txt .\classes\   2>>nul 1>>nul
-jar cf ./lib/scalabpe-plugin-%pluginname%-%pluginversion%.jar -C ./classes/  ./jvmdbbroker/plugin -C ./classes/ ./jvmdbbroker.plugins.conf -C ./classes/ ./release_notes.txt
