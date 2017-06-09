148547e6d1294dbd98df686d2486e67bc7250ee3
bruceran <bruce_ran@sina.com>
2017-06-09 10:28:39 +0800

    调整src下的目录为按包名命令目录，以便scala ide中可以准确定位到类

--- build.bat
+++ build.bat
@@ -1,19 +1,19 @@
 @echo off
 
-set version=1.2.3
-set pluginversion=1.2.3
+set version=1.2.4
+set pluginversion=1.2.4
 
 if "%1"=="" goto build
 if "%1"=="clean" goto clean
 
 :build
 mkdir classes\scalabpe\plugin\  2>>nul 1>>nul
-call scalac -deprecation -encoding UTF-8 -cp ".\lib\*;.\lib\log\*;." -d ".\classes"   src\*.scala src\plugin\*.scala src\plugin\http\*.scala src\plugin\cache\*.scala
-rem call scalac -deprecation -encoding UTF-8 -cp ".\lib\*;." -d ".\classes"   src\plugin\cache\*
-copy /Y src\plugin\scalabpe.plugins.conf .\classes\  2>>nul 1>>nul 
-copy /Y src\release_notes.txt .\classes\  2>>nul 1>>nul
+call scalac -deprecation -encoding UTF-8 -cp ".\lib\*;.\lib\log\*;." -d ".\classes"   src\scalabpe\core\*.scala src\scalabpe\plugin\*.scala src\scalabpe\plugin\http\*.scala src\scalabpe\plugin\cache\*.scala
+rem call scalac -deprecation -encoding UTF-8 -cp ".\lib\*;." -d ".\classes"   src\scalabpe\plugin\cache\*
+copy /Y src\scalabpe\plugin\scalabpe.plugins.conf .\classes\  2>>nul 1>>nul 
+copy /Y src\scalabpe\core\release_notes.txt .\classes\  2>>nul 1>>nul
 jar cf .\lib\scalabpe-core-%version%.jar -C .\classes\  .\scalabpe\core -C .\classes\ .\release_notes.txt
-copy /Y src\plugin\release_notes.txt .\classes\  2>>nul 1>>nul
+copy /Y src\scalabpe\plugin\release_notes.txt .\classes\  2>>nul 1>>nul
 jar cf .\lib\scalabpe-plugins-%pluginversion%.jar -C .\classes\  .\scalabpe\plugin -C .\classes\ .\scalabpe.plugins.conf -C .\classes\ .\release_notes.txt
 goto end
 
